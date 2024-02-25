1f18efb1fd2742823afa4dda10155063d750385f
aleksey <eremeev.dev@gmail.com>
2024-02-25 22:02:29 +0300

    move DefaultUrlInfoFetcher into fetchers.py

---
+++ fetchers.py
@@ -0,0 +1,24 @@
+import trafilatura
+
+from core import TextAnalyzer, UrlInfo
+
+
+class DefaultUrlInfoFetcher:
+    def __init__(self, analyzer: TextAnalyzer):
+        self._analyzer = analyzer
+
+    def get_info(self, url: str) -> UrlInfo | None:
+        downloaded = trafilatura.fetch_url(url)
+
+        meta: trafilatura.metadata.Document = trafilatura.extract_metadata(downloaded)
+        text: str = trafilatura.extract(downloaded)
+
+        text_info = self._analyzer.get_info(text)
+
+        url_info = UrlInfo(
+            url=url,
+            title=meta.title,
+            tags=text_info.tags
+        )
+
+        return url_info
