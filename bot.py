08170a6210ce8802501b9004d7cbda4878661169
aleksey <eremeev.dev@gmail.com>
2024-02-25 17:34:28 +0300

    move url extract logic into url.py

--- bot.py
+++ bot.py
@@ -1,47 +1,7 @@
-from typing import Protocol, Optional, List
-
 import telebot
 
 from core import UrlHandler
-import re
-
-
-class NoUrlException(Exception):
-	pass
-
-
-class UrlExtractor(Protocol):
-	def extract(self, message: telebot.types.Message) -> Optional[str]:
-		...
-
-
-class UrlFromTextExtractor:
-	def extract(self, message: telebot.types.Message) -> Optional[str]:
-		url_pattern = r'(https?://[^\s]+)'
-		url_match = re.search(url_pattern, message.text)
-		if url_match:
-			return url_match.group(0)
-
-
-class UrlFromForwardExtractor:
-	def extract(self, message: telebot.types.Message) -> Optional[str]:
-		if message.forward_from_message_id is None:
-			return
-
-		return f'https://t.me/{message.forward_from_chat.username}/{message.forward_from_message_id}'
-
-
-class UrlExtractorContext:
-	def __init__(self, strategies: List[UrlExtractor]) -> None:
-		self._strategies = strategies
-
-	def extract_url(self, message: telebot.types.Message) -> str:
-		for strategy in self._strategies:
-			url = strategy.extract(message)
-			if url:
-				return url
-
-		raise NoUrlException(message.text)
+from url import UrlExtractorContext
 
 
 class Bot:
